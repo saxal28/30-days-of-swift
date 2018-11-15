d1937042a1aee49bd4f5e4350070d464e4062080
Alan Sax <alan@lelander.com>
2018-11-14 18:50:05 -0600

    user location

--- FindingUserLocation/FindingUserLocation/ViewController.swift
+++ FindingUserLocation/FindingUserLocation/ViewController.swift
@@ -8,33 +8,53 @@
 
 import UIKit
 import CoreLocation
+import MapKit
 
-class ViewController: UIViewController, CLLocationManagerDelegate {
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
     
+    @IBOutlet weak var map: MKMapView!
     var locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
+        // =================================
+        // SETTING UP USER LOCATION
+        // =================================
         
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestWhenInUseAuthorization()
         locationManager.startUpdatingLocation()
+        
     }
     
+    // =================================
+    // INIT MAP
+    // =================================
+    
+    func initMap(userLocation: CLLocation) {
+        
+        let latitude: CLLocationDegrees = userLocation.coordinate.latitude
+        let longitude: CLLocationDegrees = userLocation.coordinate.longitude
+        
+        let latitudeDelta: CLLocationDegrees = 0.005;
+        let longitudeDelta: CLLocationDegrees = 0.005;
+        
+        let span: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: latitudeDelta, longitudeDelta: longitudeDelta)
+        let location: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        
+        let region = MKCoordinateRegion(center: location, span: span)
+        
+        map.setRegion(region, animated: true)
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
+    //FIRES EVERTTIME THE USERS LOCATION CHANGES
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         
         let userLocation: CLLocation = locations[0]
-        let latitude = userLocation.coordinate.latitude
-        let longitude = userLocation.coordinate.longitude
+        initMap(userLocation: userLocation)
     }
 
 
