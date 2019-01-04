96f6fdb762d94bf9e14c27723cbcd57e6a534547
Alan Sax <alan@lelander.com>
2019-01-04 12:47:01 -0600

    added icons for lol

--- SoundShaker/SoundShaker/ViewController.swift
+++ SoundShaker/SoundShaker/ViewController.swift
@@ -7,15 +7,37 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player = AVAudioPlayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-    func setupShake(){
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        if event?.subtype == UIEventSubtype.motionShake {
+            print("SHAKES!");
+            playRandomSound()
+        }
+    }
+    
+    func playRandomSound() {
+        let number = arc4random_uniform(9) + 1
+        let randomSound = "sound_\(number)"
+        let audioPath = Bundle.main.path(forResource: randomSound, ofType: "mp3")
+    
+        
+        do {
+            try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: audioPath!))
+            player.play()
+        } catch {
+            print("ERROR", error, randomSound)
+        }
+
     }
 
 }
